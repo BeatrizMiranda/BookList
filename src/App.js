58b05cd70d00c74797a1f920d18515692f1f40e5
Beatriz Miranda <beatriz.miranda@acct.global>
2019-12-27 15:55:52 -0300

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
